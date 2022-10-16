a66183b679c8a9e7e94c4f456af487b306dfeb4f
Martyn8 <44020154+Martyn8@users.noreply.github.com>
2022-10-16 11:49:54 +0200

    css improvement, unit conversion

--- src/index.js
+++ src/index.js
@@ -17,6 +17,13 @@ dayTime.innerHTML = dayTimeFormatted;
 
 //WEATHER API
 let apiKey = "210d99196a88b9257ed8cb3535a0a0c5";
+let celsiusTemp = null;
+
+startLocation();
+function startLocation() {
+  let startUrl = `https://api.openweathermap.org/data/2.5/weather?q=Warsaw&appid=${apiKey}&units=metric`;
+  axios.get(startUrl).then(displayWeather);
+}
 
 let submitButton = document.querySelector("form");
 
@@ -59,10 +66,12 @@ locationButton.addEventListener("click", function (event) {
 
 function displayWeather(response) {
   let temperatureDispNow = document.querySelector("#current-temp");
-  let temperatureDispDay = document.querySelector("#min-max-temp");
+  // let temperatureDispDay = document.querySelector("#min-max-temp");
   let currentWInd = document.querySelector("#wind-speed");
   let icon = document.querySelector("#today-icon");
-  let currentTemp = Math.round(response.data.main.temp);
+  celsiusTemp = response.data.main.temp;
+  let currentTemp = Math.round(celsiusTemp);
+  let description = document.querySelector("#desc");
   let maxTemp = Math.round(response.data.main.temp_max);
   let minTemp = Math.round(response.data.main.temp_min);
   let detail = response.data.weather[0].description;
@@ -71,8 +80,9 @@ function displayWeather(response) {
   console.log(response.data);
   console.log(detail);
 
-  temperatureDispNow.innerHTML = `${currentTemp}°C  <br/>${detail}`;
-  temperatureDispDay.innerHTML = `${maxTemp}°C / ${minTemp}°C`;
+  temperatureDispNow.innerHTML = `${currentTemp}`;
+  description.innerHTML = `${detail}`;
+  //temperatureDispDay.innerHTML = `${maxTemp}°C / ${minTemp}°C`;
   currentWInd.innerHTML = `${wind} `;
   console.log(icon);
   icon.setAttribute(
@@ -83,3 +93,29 @@ function displayWeather(response) {
 }
 
 //UNIT CONVESION
+
+let fahrenheitLink = document.querySelector("#fahrenheit-conv");
+fahrenheitLink.addEventListener("click", convertCToF);
+
+let celsiusLink = document.querySelector("#celsius-conv");
+celsiusLink.addEventListener("click", convertFToC);
+
+function convertCToF(event) {
+  event.preventDefault();
+
+  let convertedTemp = document.querySelector("#current-temp");
+  fahrenheitTemp = Math.round(celsiusTemp * (9 / 5) + 32);
+  console.log(fahrenheitTemp);
+  convertedTemp.innerHTML = fahrenheitTemp;
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+}
+
+function convertFToC(event) {
+  event.preventDefault();
+
+  let convertedTemp = document.querySelector("#current-temp");
+  convertedTemp.innerHTML = Math.round(celsiusTemp);
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+}
